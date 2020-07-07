fc147b7c6761049bf43f6584df473c2782829bbe
Oliver Julian Taner <oliver-tanner@outlook.com>
2020-07-07 21:35:20 +0200

    Finish Chapter 09

--- 09_playing_with_processes/advanced.ex
+++ 09_playing_with_processes/advanced.ex
@@ -3,8 +3,9 @@
 defmodule Bounce do
   def report do
     receive do
-      msg -> IO.puts("Received #{msg}")
-      report()
+      msg ->
+        IO.puts("Received #{msg}")
+        report()
     end
   end
 end
@@ -28,3 +29,83 @@ end
 # Process.whereis(:bounde)
 # Process.unregister(:bounce)
 # ```
+
+# Processes can talk between each other
+
+defmodule Drop do
+  def drop do
+    receive do
+      {from, planemo, distance} ->
+        send(from, {planemo, distance, fall_velocity(planemo, distance)})
+        drop()
+    end
+  end
+
+  defp fall_velocity(:earth, distance) when distance >= 0 do
+    :math.sqrt(2 * 9.8 * distance)
+  end
+
+  defp fall_velocity(:moon, distance) when distance >= 0 do
+    :math.sqrt(2 * 1.6 * distance)
+  end
+
+  defp fall_velocity(:mars, distance) when distance >= 0 do
+    :math.sqrt(2 * 3.71 * distance)
+  end
+end
+
+defmodule MphDrop do
+  def mph_drop do
+    drop_id = spawn(Drop, :drop, [])
+    convert(drop_id)
+  end
+
+  def convert(drop_id) do
+    receive do
+      {planemo, distance} ->
+        send(drop_id, {self(), planemo, distance})
+        convert(drop_id)
+
+      {planemo, distance, velocity} ->
+        mph_velocity = 2.23693629 * velocity
+        IO.write("On #{planemo}, a fall of #{distance} meters ")
+        IO.puts("yields a velocity of #{mph_velocity} mph.")
+        convert(drop_id)
+    end
+  end
+end
+
+# In IEX execute
+# ```
+# pid1 = spawn(MphDrop, :mph_drop, [])
+# send(pid1, {:earth, 20})
+# ```
+#
+# This will now create a process that delegates work to another process.
+
+# If you want to be able to intercept when a process fails instead of silently ignoring it, do the following trapping.
+
+defmodule MphDropWithFailureTrap do
+  def mph_drop do
+    Process.flag(:trap_exit, true)
+    drop_id = spawn_link(Drop, :drop, [])
+    convert(drop_id)
+  end
+
+  def convert(drop_id) do
+    receive do
+      {:EXIT, pid, reason} ->
+        IO.puts("Failure: #{inspect(pid)} #{inspect(reason)}")
+      {planemo, distance} ->
+        send(drop_id, {self(), planemo, distance})
+        convert(drop_id)
+
+      {planemo, distance, velocity} ->
+        mph_velocity = 2.23693629 * velocity
+        IO.write("On #{planemo}, a fall of #{distance} meters ")
+        IO.puts("yields a velocity of #{mph_velocity} mph.")
+        convert(drop_id)
+    end
+  end
+end
+
